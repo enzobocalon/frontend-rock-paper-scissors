71a804c793d66ba9c3a4c498aa9b3f700e5c0ae2
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-11-03 16:34:42 -0300

    style: removed unused css code

--- src/components/GamePick/style.ts
+++ src/components/GamePick/style.ts
@@ -30,28 +30,6 @@ export const Border = styled.div<IProps>`
   }
 `
 
-export const Play = styled.div`
-  background-color: white;
-  width: 180px;
-  height: 180px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  box-shadow: inset 1px 8px 5px 2px rgb(0 0 0 / 20%), 2px 2px 5px rgb(0 0 0 / 10%);
-  transition: all .3s ease;
-
-  &:hover{
-    background-color: rgba(236, 231, 231, 1);
-    transition: all .3s ease;
-  }
-
-  @media screen and (max-width: 450px){
-    width: 130px;
-    height: 130px;
-  }
-`
-
 export const FirstRow = styled.div<IProps>`
   display: flex;
   align-items: center;
